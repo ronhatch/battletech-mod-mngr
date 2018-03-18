a127b4950e3087682c61f395b5fc641f7aa95211
Ron Hatch <35791609+ronhatch@users.noreply.github.com>
2018-03-18 02:30:34 -0700

    Refactored to put all test file operations into file-steps.rb.

--- features/step-definitions/file-steps.rb
+++ features/step-definitions/file-steps.rb
@@ -1,24 +1,26 @@
+TempDir = 'temp-testing'
+
 # Any scenario that uses files needs the temp directory.
 Before('@files') do
-  unless Dir.exist?('temp-testing') then
-    Dir.mkdir('temp-testing')
+  unless Dir.exist?(TempDir) then
+    Dir.mkdir(TempDir)
   end
-  unless File.exist?('temp-testing/Readme.txt') then
+  unless File.exist?("#{TempDir}/Readme.txt") then
     text  = "This entire directory is used only for testing.\n"
     text += "These files should all be deleted at the end of each test,\n"
     text += "  but if they are still here you can safely delete them manually.\n"
-    File.write('temp-testing/Readme.txt', text)
+    File.write("#{TempDir}/Readme.txt", text)
   end
 end
 
 # And if we've made the temp directory, we need to clean it up.
 After('@files') do
-  Dir.foreach('temp-testing') do |f|
-    name = "temp-testing/#{f}"
+  Dir.foreach(TempDir) do |f|
+    name = "#{TempDir}/#{f}"
     if File.file?(name) then
       File.delete(name)
     end
   end
-  Dir.delete('temp-testing')
+  Dir.delete(TempDir)
 end
 
